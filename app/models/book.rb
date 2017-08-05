7585ff6f6cd8a12e31945649f8cd16d127fe90f3
Izabela Styrylska <izabela.styrylska@gmail.com>
2017-08-05 15:32:10 +0200

    errors styled

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,10 @@
 class Book < ActiveRecord::Base
   belongs_to :owner, :class_name => "User"
   belongs_to :rent_by, :class_name => "User"
+  validates_presence_of :author, :message => "Ksiązka musi mieć podanego autora"
+  validates_presence_of :name, :message => "Ksiązka musi mieć podany tytuł"
+
+
 
   # def self.search(search)
   # 	if search

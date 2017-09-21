dfd44c1be110a4987c37ef970c5b7b3918f794d6
Izabela Styrylska <izabela.styrylska@gmail.com>
2017-09-21 09:53:30 +0200

    sql error fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :owned, :class_name => "Book", :foreign_key  => "owned_id"
-  has_many :rented, :class_name => "Book", :foreign_key  => "rented_id"
+  # has_many :owned, :class_name => "Book", :foreign_key  => "owned_id"
+  # has_many :rented, :class_name => "Book", :foreign_key  => "rented_id"
+   has_many :owned, :class_name => "Book", :foreign_key  => "owner_id"
+  has_many :rented, :class_name => "Book", :foreign_key  => "rent_by_id"
 end
